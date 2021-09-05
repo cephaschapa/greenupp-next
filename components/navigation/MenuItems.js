b42a5f1069d8d8086699ccae699207854989f49e
cephaschapa <cephaschapa@gmail.com>
2021-09-05 03:24:38 +0200

    Added Drawer Menu

--- components/navigation/MenuItems.js
+++ components/navigation/MenuItems.js
@@ -2,7 +2,7 @@ import { QuestionMarkCircleIcon } from "@heroicons/react/outline"
 
 function MenuItems({icon, title}) {
     return (
-            <div flex className="flex flex-col items-center justify-center border border-white p-2 rounded-2xl text-gray-100 text-sm">
+            <div flex className="flex flex-col items-center justify-center border-2 border-white p-2 rounded-2xl text-gray-100 text-sm">
                 {icon}
                 <p className="">{title}</p>
             </div>
