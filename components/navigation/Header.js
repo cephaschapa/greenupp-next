4106f6788077b749f6ea87f0e4e167d16bc029da
cephaschapa <cephaschapa@gmail.com>
2021-09-10 21:07:36 +0200

    Drawer menu update

--- components/navigation/Header.js
+++ components/navigation/Header.js
@@ -4,6 +4,7 @@ import {useState, useEffect} from 'react'
 import Acrylic from 'react-acrylic'
 import MenuItems from './MenuItems'
 import Link from 'next/link'
+import { MenuAlt3Icon, TranslateIcon } from '@heroicons/react/outline'
 function Header() {
     const [drawer, setOpenDrawer] = useState(false)
     const [opacity, setOpacity] = useState(false)
@@ -28,33 +29,37 @@ function Header() {
             <div className="w-48 relative h-full">
                 <Link href="/"><Image src="/logo.png" layout="fill"/></Link>
             </div>
-            <div className="">
-                <MenuAlt4Icon onClick={openDrawer} className="h-8 w-8 text-gray-500"/>
+            <div className="bg-gray-200 p-2 rounded-full" onClick={openDrawer}>
+                <MenuAlt3Icon className="h-8 w-8 text-gray-500"/>
             </div>
         
         </header>      
-        {
-            drawer ? 
-            
-            <div className="fixed text-gray-600 h-full w-full bg-white z-50 p-2 flex flex-col bg-gradient-to-bl from-white via-white to-gray-200">
+            <div className={`${drawer? 'inline h-screen': 'hidden'} fixed top-0 transition transform duration-150 text-gray-600 w-full bg-white z-50 p-2 flex flex-col`}>
             {/* Header */}
-                <div className="flex justify-end pt-4">
-                    <XIcon onClick={closeDrawer} className="h-10 -10 text-gray-600 "/>
+                <div className="flex justify-between pt-4">
+                    <TranslateIcon onClick={closeDrawer} className="h-12 bg-gray-200 p-2 text-gray-600 rounded-full"/>
+                    <XIcon onClick={closeDrawer} className="h-12 bg-gray-200 p-2 text-gray-600 rounded-full"/>
                 </div>
-                <div className="flex flex-col items-center mt-10 h-full">
-                    <p className="text-3xl p-4">MENU</p>
-                    <div className="grid grid-cols-3 space-x-3">
+                <div className="flex flex-col items-center mt-5 h-full">
+                    <p className="text-3xl font-bold p-4">MENU</p>
+                    {/* <div className="grid grid-cols-3 space-x-3">
                         <MenuItems icon={<QuestionMarkCircleIcon className="h-10 w-10 text-center"/>} title="The Problem" route="./theproblem"/>
                         <MenuItems icon={<BeakerIcon className="h-10 w-10 text-center"/>} title="The Solution"/>
                         <MenuItems icon={<BadgeCheckIcon className="h-10 w-10 text-center"/>} title="The Impact"/>
-                    </div>
-                    <div className="flex flex-col items-center py-10 space-y-5 w-full">
-                        <p className="font-bold">About</p>
-                        <p className="font-bold">Contacts</p>
+                    </div> */}
+                    <div className="flex flex-col items-center space-y-4 w-full mt-5">
+                        <p className="font-bold">App</p>
+                        <div className="divider bg-gray-200 w-1/2 h-0.5"></div>
+                        <p className="font-bold">Research & Findings</p>
+                        <div className="divider bg-gray-200 w-1/2 h-0.5"></div>
                         <p className="font-bold">News</p>
-                        <p className="font-bold">Partners</p>
-                        <p className="font-bold">Clients</p>
-                        <div className="w-full space-x-4 flex justify-around">
+                        <div className="divider bg-gray-200 w-1/2 h-0.5"></div>
+                        <p className="font-bold">About</p>
+                        <div className="divider bg-gray-200 w-1/2 h-0.5"></div>
+                        <p className="font-bold">Careers</p>
+                        <div className="divider bg-gray-200 w-1/2 h-0.5"></div>
+                        <p className="font-bold">Support</p>
+                        <div className="w-full space-x-4 flex justify-around items-end bottom-8 absolute p-3">
                             <button className="rounded-full shadow-md border border-gray-200 flex-grow font-bold p-4 bg-white text-black">Sign in</button>
                             <button className="rounded-full  flex-grow font-bold shadow-md border border-gray-200 p-4 bg-white text-black ">Sign up</button>
                         </div>
@@ -62,8 +67,8 @@ function Header() {
                 </div>
             {/* Body */}
             {/* Footer */}
-        </div> : null
-        }
+        </div>
+        
        </div> 
     )
 }
